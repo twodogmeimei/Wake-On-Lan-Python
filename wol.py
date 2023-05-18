d5f80746b0c2d3f4907a7e496b23fad594d317a9
jack-d-chen <jackdchen@gootwodog.com>
2023-05-18 23:14:23 +0800

    Revert "add new line"
    This reverts commit ded0a110e9f0fe1e5cd6f6eb185fb375f523e5f4.

--- wol.py
+++ wol.py
@@ -188,7 +188,6 @@ class WakeOnLan(object):
     @staticmethod
     def usage() -> None:
         print(
-            '\n'
             'Usage: wol.py [-p] [hostname|list]\n'
             '\n'
             '-h            Print this text\n\n'
